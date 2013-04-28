d77e7ed2db94059b77fbe89ae7f26f75fb90ce1c
Grant Mathews <johnfn@gmail.com>
2013-04-28 16:13:40 -0700

    Mostly fix HUD.

--- src/HUD.hx
+++ src/HUD.hx
@@ -33,14 +33,14 @@ class HUD extends Entity {
 
     player = p;
 
-    healthbar = new Healthbar(50, 50, p);
+    healthbar = new Healthbar(0, 0, p);
     HXP.scene.add(healthbar);
 
-    gunText = new FancyText("Gun:{255,195,195} *DerpGun*", 50, 85);
+    gunText = new FancyText("Gun:{255,195,195} *Unarmed!*", 30, 35);
     textHolder = new Entity();
     textHolder.graphic = gunText;
 
-    gunTextShadow = new FancyText("{0,0,0}*Gun: DerpGun*", gunText.x + 2, gunText.y + 2);
+    gunTextShadow = new FancyText("{0,0,0}*Gun: Unarmed!*", gunText.x + 2, gunText.y + 2);
     textShadowHolder = new Entity();
     textShadowHolder.graphic = gunTextShadow;
     HXP.scene.add(textShadowHolder);
@@ -53,21 +53,28 @@ class HUD extends Entity {
 
     gunIcon = new Entity();
     gunIcon.graphic = spritemap;
-    gunIcon.x = 25;
-    gunIcon.y = 80;
+    gunIcon.x = 0;
+    gunIcon.y = 30;
 
     HXP.scene.add(gunIcon);
 
-    coinText = new FancyText("Coins: 0", HXP.width - 100, 50, 100, 50, {align: nme.text.TextFormatAlign.RIGHT, color: 0xffffff});
+    coinText = new FancyText("Coins: 0", HXP.width - 100, 0, 100, 50, {align: nme.text.TextFormatAlign.RIGHT, color: 0xffffff});
     coinTextHolder = new Entity();
     coinTextHolder.graphic = coinText;
 
-    coinTextShadow = new FancyText("Coins: 0", HXP.width - 100 + 2, 50 + 2, 100, 50, {align: nme.text.TextFormatAlign.RIGHT, color: 0});
+    coinTextShadow = new FancyText("Coins: 0", HXP.width - 100 + 2, 0 + 2, 100, 50, {align: nme.text.TextFormatAlign.RIGHT, color: 0});
     coinTextShadowHolder = new Entity();
     coinTextShadowHolder.graphic = coinTextShadow;
 
     HXP.scene.add(coinTextShadowHolder);
     HXP.scene.add(coinTextHolder);
+
+    setGunType("Unarmed!");
+  }
+
+  public function setGunType(s:String) {
+    gunText.text = 'Gun: $s';
+    gunTextShadow.text = '{0,0,0}*Gun: $s*';
   }
 
   public override function update() {
